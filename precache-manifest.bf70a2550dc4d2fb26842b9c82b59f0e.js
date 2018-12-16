5ed7f1b1d8cea3b6b419f1330b1dea424e8d56a1
Eugeny <aladof@ya.ru>
2018-12-16 22:27:46 +0300

    list books

---
+++ precache-manifest.bf70a2550dc4d2fb26842b9c82b59f0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6033ef8ddddad478160",
+    "url": "/static/js/main.e6033ef8.chunk.js"
+  },
+  {
+    "revision": "6c9f66824607b2f7ec69",
+    "url": "/static/js/1.6c9f6682.chunk.js"
+  },
+  {
+    "revision": "e6033ef8ddddad478160",
+    "url": "/static/css/main.f4da8491.chunk.css"
+  },
+  {
+    "revision": "6728af3182a2c62596f35773b84a53de",
+    "url": "/index.html"
+  }
+];
