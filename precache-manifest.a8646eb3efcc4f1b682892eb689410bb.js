b4f2791847bcfbdc2e6b7321995c9e9fe917542e
Eugeny <aladof@ya.ru>
2019-02-16 22:46:22 +0300

    Updates

--- precache-manifest.a8646eb3efcc4f1b682892eb689410bb.js
+++ precache-manifest.a8646eb3efcc4f1b682892eb689410bb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/book-reader/static/js/runtime~main.8c785f94.js"
   },
   {
-    "revision": "710a5c36f09ce1c5b470",
-    "url": "/book-reader/static/js/main.710a5c36.chunk.js"
+    "revision": "6935e8d0edc3f0de1c30",
+    "url": "/book-reader/static/js/main.6935e8d0.chunk.js"
   },
   {
     "revision": "c12b74e24ba06479f89a",
     "url": "/book-reader/static/js/1.c12b74e2.chunk.js"
   },
   {
-    "revision": "710a5c36f09ce1c5b470",
+    "revision": "6935e8d0edc3f0de1c30",
     "url": "/book-reader/static/css/main.83dbc8fc.chunk.css"
   },
   {
-    "revision": "fce810fcf3d777a3e35535e5ed562e06",
+    "revision": "c349fa98f9aada8dae328677e3b22d83",
     "url": "/book-reader/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/book-reader/precache-manifest.33e8c3e53336bf8007d9bbd9b5e04b7c.js"
+  "/book-reader/precache-manifest.a8646eb3efcc4f1b682892eb689410bb.js"
 );
 
 workbox.clientsClaim();
